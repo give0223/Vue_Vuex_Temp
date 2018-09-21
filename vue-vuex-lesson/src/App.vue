c9499ad6dd8bc89c1e6c5605fc4d0e483f5c26c0
Vincent_Lin <give0223@gmail.com>
2018-09-21 10:43:00 +0800

    vuex learning

---
+++ vue-vuex-lesson/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{totalcomut}}</h1>
+    <button @click="addStoreCount">Add</button>
+  </div>  
+</template>
+
+<script>
+  import {mapState, mapMutations} from 'vuex';
+  export default {
+    data(){
+      return{
+        localCount: 10,
+      };
+    },
+    computed:{
+      // count(){
+      //   return this.$store.state.count;
+      // },
+      ...mapState({
+        totalcomut(state){
+          return this.localCount + state.count;
+        },
+      }),
+    },
+    methods:{
+      // addcount(){
+      //   // this.$store.commit('addCount');
+      // },
+      ...mapMutations(['addStoreCount']),
+    },
+  }
+</script>
+
+
+<style>
+</style>
